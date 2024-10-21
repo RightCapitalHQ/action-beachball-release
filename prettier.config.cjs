32e404c346aa1472059630018ac9296e24258bb8
Jiahao Guo <im@pyonpyon.today>
2024-10-21 15:08:03 +0800

    chore: remove unnecessary Prettier plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,2 @@
-const baseConfig = require('@rightcapital/prettier-config');
-
 /** @type {import('prettier').Options} */
-const config = {
-  ...baseConfig,
-  plugins: ['prettier-plugin-packagejson'],
-};
-
-module.exports = config;
+module.exports = require('@rightcapital/prettier-config');
